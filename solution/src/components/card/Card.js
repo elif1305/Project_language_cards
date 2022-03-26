7e7f9056928b2e91530da9985f1bb4b007489829
Ella <ekosardata@gmail.com>
2022-03-26 23:48:16 +0100

    news

--- solution/src/components/card/Card.js
+++ solution/src/components/card/Card.js
@@ -9,7 +9,7 @@ const Card = () => {
       <h1 className="language-title">Languages</h1>
       <div className="cards-container">
         {languages.map((item, index) => {
-          return <Item card={item} key={index} />;
+          return <Item card={item} key={index} />;  // key = {id} bestpractice te yazilir fakat bu proede d olmadigi icin index yazildi. burada card uyerine baska bir seyde yazilablirdi.
         })}
       </div>
     </div>
@@ -17,3 +17,17 @@ const Card = () => {
 };
 
 export default Card;
+
+
+// asagidaki gibiyapilirsa ucun yol olurdu ve tek tek her bir kartin bu sekilde yapilmasi gerekiridi.
+{/* <div>
+ <img src={item.img} alt="" />
+ <h3> {item.name} </h3>
+</div> */}
+
+// kisa yolu bu sekildedir (kodda oldugu gibi)
+{/* <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} key={index} />;
+        })}
+</div> */}
--- solution/src/helpers/data.js
+++ solution/src/helpers/data.js
@@ -10,42 +10,42 @@ import html from "../assets/html.png";
 export const languages = [
   {
     name: "javascript",
-    img: javascript,
+    img: javascript,   // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["Website Development", "1995", "Brendan Eich"],
   },
   {
     name: "html",
-    img: html,
+    img: html,    // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["Website Development", "1993", "Tim Berners-Lee"],
   },
   {
     name: "css",
-    img: css,
+    img: css,   // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["Website Design", "1994", "Håkon Wium Lie"],
   },
   {
     name: "python",
-    img: python,
+    img: python,   // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["AI", "1980", "Guido van Rossum"],
   },
   {
     name: "java",
-    img: java,
+    img: java,   // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["BackEnd", "1990s", "Sun Microsystems"],
   },
   {
     name: "c++",
-    img: cpp,
+    img: cpp,   // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["Game Dev.", "1980s", "Bjarne Stroustrup"],
   },
   {
     name: "go",
-    img: go,
+    img: go,   // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["Network", "2009", "Robert Griesemer"],
   },
   {
     name: "php",
-    img: php,
+    img: php,    // buraada suslu parantez icinde yazmadik , cunku img  in kendisi zaten bir suslu (obje) icinde.
     options: ["BackEnd", "1995", "	Rasmus Lerdorf"],
   },
 ];
